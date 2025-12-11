3093c8aa28fcb18aaac81415b186b50ecd8f085a
Arthur Guillemin <aguillemin291@gmail.com>
2025-12-11 16:44:08 +0100

    Mauvais commit

---
+++ client/src/bad.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function Bad() {
+  const [mavar, setState] = useState('');
+  return (
+    <>
+    </>
+  );
+}
+
+export default Composant;
---
+++ client/src/footer.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function Footer() {
+  const [mavar, setState] = useState('');
+  return (
+    <>
+    </>
+  );
+}
+
+export default footer;
